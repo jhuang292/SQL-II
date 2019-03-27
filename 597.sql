486bf62714461baf3037e16bf07596cf3276695c
jhuang292 <jhuang292@wisc.edu>
2019-03-27 02:13:07 -0500

    friend requestsI: overall acceptance rate

---
+++ 597.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+SELECT ROUND(IFNULL((SELECT COUNT(*) FROM (SELECT DISTINCT requester_id, accepter_id FROM request_accepted) A) / 
+(SELECT COUNT(*) FROM (SELECT DISTINCT sender_id, send_to_id FROM friend_request) B),0),2) AS accept_rate
