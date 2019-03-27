47fd496ec4c0c065c0074ca714a449ab103f207b
jhuang292 <jhuang292@wisc.edu>
2019-03-27 01:51:46 -0500

    find customer referee

---
+++ 584.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT name
+FROM customer 
+WHERE id NOT IN (
+                            SELECT id
+                            FROM customer
+                            WHERE referee_id = '2'
+                            );
