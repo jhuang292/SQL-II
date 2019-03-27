cfbf5f1b8f8e2b964dcca792e423618003baeaa9
jhuang292 <jhuang292@wisc.edu>
2019-03-27 01:42:04 -0500

    exchange seats

---
+++ 626.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT CASE
+             WHEN MOD(id, 2) = 1 AND id != tb.counts THEN id + 1
+             WHEN MOD(id, 2) = 1 AND id = tb.counts THEN id
+             ELSE id -1
+        END AS id, student
+FROM seat, (
+                SELECT COUNT(*) AS counts
+                FROM seat
+                ) tb
+ORDER BY id ASC;
